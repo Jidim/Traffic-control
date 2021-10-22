f8bdc5c3c7edced50389f333b7db72ae20d41e3b
Jidim <78730440+Jidim@users.noreply.github.com>
2021-10-23 02:30:57 +0300

    2

--- GetInfo.js
+++ GetInfo.js
@@ -1,14 +1,19 @@
 async function GetPhase()
 {
+    var phase;
+    var status;
     var id = parseInt(document.getElementById("dk").value);
-    var requestURL = "https://api.via-dolorosa.ru/rc/26971/status";
+    var requestURL = "https://api.via-dolorosa.ru/rc/" + id.toString() + "/status";
     let response = await fetch(requestURL);
     if (response.ok) {
         let json = await response.text();
         var dk = JSON.parse(json);
-        console.log(dk["current_phase_id"]);
+        phase = dk["current_phase_id"];
+        status = dk["status"];
     }
     else {
         console.log("Ошибка HTTP " + response.status);
     }
+    document.getElementById("status").value = status;
+    document.getElementById("phase").value = phase;
 }
