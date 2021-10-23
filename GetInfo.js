d1e07e2bae518e53ab2ba0a8103e6dca7e5f4f05
Jidim <78730440+Jidim@users.noreply.github.com>
2021-10-24 00:57:52 +0300

    7

--- GetInfo.js
+++ GetInfo.js
@@ -6,16 +6,31 @@ async function GetInfo(id) {
     if (response.ok) {
         let json = await response.json();
        var problem = document.getElementById("problems" + id);
+       // Обработка возможных ошибок работы ДК
         if (json["mode"] != json["real_mode"])
             problem.value = "Несоответствие модов";
         if (json["program_id"] != json["real_program_id"])
             problem.value = "Несоответствие программ";
+
         document.getElementById("start" + id).value = json["program_created_at"];
         document.getElementById("end" + id).value = json["changed_time_at"];
         var cycleTime = parseInt(json["t_cycle"]);
         document.getElementById("cycleTime" + id).value = cycleTime.toString();
-        if (document.getElementById("time" + id.toString()).value == "")
-            PhaseTime(id);
+        // var time = document.getElementById("time" + id.toString()).value;
+        // if (time == ''){
+        //     document.getElementById("time" + id.toString()).value = "1";
+        // }
+        // else {
+        //     // var line;
+        //     // var times = (time.toString()).split(" ");
+        //     // var cur = [];
+        //     // for (var i = 0; i < times.lenght; i++)
+        //     //     cur.push(parseInt(times[i]));
+        //     // for (var i = 0; i < cur.length; i++)
+        //     //      line += cur[i].toString() + " ";
+        //     //      document.getElementById("time" + id.toString()).value = line;
+        //     console.log("Yes");
+        // }
     }
     else {
         console.log("Ошибка HTTP " + response.status);
@@ -43,8 +58,10 @@ async function GetPhase(id)
             var problem = document.getElementById("problems" + id);
             phase = commits['current_phase_id'].toString();
             status = commits['status'].toString();
+            // Обработка статуса ДК
             if (status != "OK")
                 problem.value = "Неправильный статус";
+                
             if (parseInt(document.getElementById("phases" + id).value) < parseInt(phase))
                 document.getElementById("phases" + id).value = phase;
             document.getElementById("status" + id).value = "статус - " + status;
@@ -60,26 +77,44 @@ async function GetPhase(id)
         document.getElementById("phase" + id).value = "Нет доступа к контроллеру";
     }
 }
+// Установка программы для ДК, пока жесткошовная
 async function SetProgramm(id) {
-    document.getElementById("button" + id).value = "Не удалось открыть файл";
-}
-
-async function PhaseTime(id) {
-    var rc = document.getElementById("cycleTime" + id.toString());
-    var times = [];
-    for (var i = 0; i <parseInt(document.getElementById("phases" + id.toString())); i++)
-        times.push();
-    for (var i = 0; i < parseInt(rc.value); i++) {
-        GetPhase(id);
-        var phase = document.getElementById("phase" + id.toString());
-        if (phase > times.length)
-            for (var i = 0; i < phase - times.length; i++)
-                times.push();
-        times[phase-1]++;
-        setTimeout(function () {}, parseInt(rc.value)*10+1);
+    var path = document.getElementById("path" + id.toString().value);
+    let file = path.files[0];
+    var buffer;
+    let reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function () {
+        buffer += reader;
     }
-    console.log(times);
+    var json = JSON.parse(buffer);
+    var data = JSON.stringify(json);
+    let xhr = new XMLHttpRequest();
+    var url = "https://api.via-dolorosa.ru/rc/"+id.toString()+"/custom_phase_program";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(data);
 }
-function SaveInfo() {
-
+// Сохранение информации о порграмме на ДК
+function save_content_to_file(id)
+{
+    let encoder = new TextEncoder();
+    var line = document.getElementById("time" + id.toString).value;
+    let content = encoder.encode(line);
+    with(document){
+     ir=createElement('iframe');
+     ir.id='ifr';
+     ir.location='about.blank';
+     ir.style.display='none';
+     body.appendChild(ir);
+      with(getElementById('ifr').contentWindow.document){
+           open("text/plain", "replace");
+           charset = "utf-8";
+           write(content);
+           close();
+           document.charset = "utf-8";
+           execCommand('SaveAs', false, id+'.txt');
+       }
+       body.removeChild(ir);
+     }
 }
